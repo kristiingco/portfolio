78ca4c6c25cfbbfac6490c74f6d8c177266fa247
Kristi <kristiana.d.ingco@gmail.com>
2022-07-06 20:12:59 -0700

    Add Navbar Skeleton

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex">
+      <div>
+        <span>k.i.</span>
+      </div>
+      <div>
+        <ul>
+          <li>about</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import HeroBanner from "../components/HeroBanner";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <NavBar />
         <HeroBanner />
       </main>
     </div>
