a41c287ce727d39e271fd97a3f76a5e43d3ce368
Kristi <kristiana.d.ingco@gmail.com>
2022-07-10 13:59:27 -0700

    Add spacing between experience and education

--- components/SectionCards.js
+++ components/SectionCards.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 const SectionCards = ({ data }) => {
   return (
     <div className="">
-      <div className="flex overflow-x-auto overflow-y-hidden gap-5 p-4 pl-10 justify-start">
+      <div className="flex overflow-x-auto overflow-y-hidden gap-5 p-4 pl-10 pb-10 justify-start">
         {data.map(({ title, subtitle, duration, description }, idx) => (
           <Card
             key={idx}
